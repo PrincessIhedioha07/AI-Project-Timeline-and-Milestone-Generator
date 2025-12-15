6e72c44810b7c6f966840ebd65aaaa4196f23e6b
Princess Ihedioha <ihediohaprincess7@gmail.com>
2025-12-15 16:42:16 +0100

    refactor: Use `server_metadata_url` for Google OAuth client configuration instead of explicit URLs.

--- app.py
+++ app.py
@@ -64,12 +64,7 @@ google = oauth.register(
     name='google',
     client_id=app.config['GOOGLE_CLIENT_ID'],
     client_secret=app.config['GOOGLE_CLIENT_SECRET'],
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openid to fetch user info
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'},
 )
 
